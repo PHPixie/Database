009bd4204b5a0f0bb97e0e494270784c2a1a71b2
Dracony <draconyster@gmail.com>
2015-03-04 12:01:29 +0100

    insert interface

--- src/PHPixie/Database/Type/SQL/Query/Type/Insert.php
+++ src/PHPixie/Database/Type/SQL/Query/Type/Insert.php
@@ -5,5 +5,7 @@ namespace PHPixie\Database\Type\SQL\Query\Type;
 interface Insert extends \PHPixie\Database\Type\SQL\Query,
                          \PHPixie\Database\Query\Type\Insert
 {
-
+    public function batchData($columns, $rows);
+    public function clearBatchData();
+    public function getBatchData();
 }
