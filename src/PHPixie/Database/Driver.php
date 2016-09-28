299c51a8f79eceda6df58e0c801afc6a982a7836
nayf <nayfania@gmail.com>
2016-09-28 22:18:52 +0300

    drop never use $config variable

--- src/PHPixie/Database/Driver.php
+++ src/PHPixie/Database/Driver.php
@@ -2,18 +2,43 @@
 
 namespace PHPixie\Database;
 
+use PHPixie\Database;
+
+/**
+ * Class Driver
+ * @package PHPixie\Database
+ */
 abstract class Driver
 {
+    /**
+     * @var Database
+     */
     protected $database;
+    /**
+     * @var mixed
+     */
     protected $conditions;
+    /**
+     * @var array
+     */
     protected $parsers =  array();
+    /**
+     * @var array
+     */
     protected $connections = array();
 
+    /**
+     * Driver constructor.
+     * @param $database Database
+     */
     public function __construct($database)
     {
         $this->database = $database;
     }
-    
+
+    /**
+     * @return mixed
+     */
     public function conditions()
     {
         if ($this->conditions === null)
@@ -22,6 +47,10 @@ abstract class Driver
         return $this->conditions;
     }
 
+    /**
+     * @param $connectionName string
+     * @return Parser
+     */
     public function parser($connectionName)
     {
         if (!isset($this->parsers[$connectionName]))
@@ -30,20 +59,55 @@ abstract class Driver
         return $this->parsers[$connectionName];
     }
 
+    /**
+     * @param string $type
+     * @param string $connectionName
+     * @return mixed
+     */
     public function query($type = 'select', $connectionName = 'default')
     {
         $connection = $this->database->get($connectionName);
-        $config     = $connection->config();
         $parser     = $this->parser($connectionName);
         $builder    = $this->queryBuilder();
 
         return $this->buildQuery($type, $connection, $parser, $builder);
     }
 
+    /**
+     * @return mixed
+     */
     abstract public function queryBuilder();
+
+    /**
+     * @return mixed
+     */
     abstract public function buildConditions();
+
+    /**
+     * @param $name string
+     * @param $config array
+     * @return mixed
+     */
     abstract public function buildConnection($name, $config);
+
+    /**
+     * @param $connectionName string
+     * @return mixed
+     */
     abstract public function buildParserInstance($connectionName);
+
+    /**
+     * @param $type string
+     * @param $connection
+     * @param $parser
+     * @param $builder
+     * @return mixed
+     */
     abstract public function buildQuery($type, $connection, $parser, $builder);
+
+    /**
+     * @param $cursor
+     * @return mixed
+     */
     abstract public function result($cursor);
 }
