a2a13f9b52937d130c87ce18c0e309d2b0962aee
Anton Kolenkov <dorantor@gmail.com>
2017-03-06 07:23:39 +0300

    Added basic support for network address operators in PosgreSQL.

--- src/PHPixie/Database/Driver/PDO/Adapter/Pgsql/Parser/Operator.php
+++ src/PHPixie/Database/Driver/PDO/Adapter/Pgsql/Parser/Operator.php
@@ -4,4 +4,16 @@ namespace PHPixie\Database\Driver\PDO\Adapter\Pgsql\Parser;
 
 class Operator extends \PHPixie\Database\Type\SQL\Parser\Operator
 {
+    /**
+     * Operator constructor.
+     *
+     * @param $database
+     * @param $fragmentParser
+     */
+    public function __construct($database, $fragmentParser)
+    {
+        $this->operators['compare'] = array_merge($this->operators['compare'], ['>>', '>>=', '<<', '<<=',]);
+
+        parent::__construct($database, $fragmentParser);
+    }
 }
